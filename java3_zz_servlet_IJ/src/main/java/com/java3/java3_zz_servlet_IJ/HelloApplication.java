72c83058ff0f61a903c6595ee61927a6ab27c56b
alexweichen22 <alexweichen22@gmail.com>
2021-03-07 15:45:58 -0500

    update

--- java3_zz_servlet_IJ/src/main/java/com/java3/java3_zz_servlet_IJ/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.java3.java3_zz_servlet_IJ;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class HelloApplication extends Application {
-
-}
